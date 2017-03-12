15bb0278b5e15bdcd85f9865a3c8d96e17f907b8
Eliran Bivas <eliran.bivas@gmail.com>
2017-03-12 15:14:20 +0200

    bitbucket pipelines parser

---
+++ parser.go
@@ -0,0 +1,42 @@
+package bitbucketpipelines
+
+import (
+	"io"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type config struct {
+	Step step `yaml:"step"`
+}
+
+type step struct {
+	Scripts []string `yaml:"script"`
+}
+
+type pipelines struct {
+	Default   []config `yaml:"default"`
+	Branches  []config `yaml:"branches,omitempty"`
+	Tags      []config `yaml:"tags,omitempty"`
+	Bookmarks []config `yaml:"bookmarks,omitempty"`
+}
+
+//PipelineDef : bitbucket pipelines definition
+type PipelineDef struct {
+	Image     string    `yaml:"image"`
+	Pipelines pipelines `yaml:"pipelines"`
+}
+
+//ReadPipelineDef : parse bitbucket pipeline input
+func ReadPipelineDef(reader io.Reader) (result PipelineDef) {
+	bytes, err := ioutil.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal(bytes, &result)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
---
+++ parser_test.go
@@ -0,0 +1,24 @@
+package bitbucketpipelines
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMarshelling(t *testing.T) {
+	data := `
+image: python:2.7
+pipelines:
+ default:
+   - step:
+      script:
+        - python --version
+        - python myScript.py
+`
+	reader := strings.NewReader(data)
+	result := ReadPipelineDef(reader)
+	assert.Equal(t, "python:2.7", result.Image, "missing image")
+	assert.Equal(t, "python --version", result.Pipelines.Default[0].Step.Scripts[0], "missing script")
+}
